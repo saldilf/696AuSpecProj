36fec7156d22a61302c95fec89d111e350c23358
Salwan Butrus <salwan@umich.edu>
2018-11-17 15:14:04 -0500

    starting proj

---
+++ AuSpectra.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 17 15:00:19 2018
+
+@author: salwanbutrus
+
+
+At a minimum, there should be two options that the user can choose to control the output of the program. 
+    1) No plot: type in file name to get info on spectrum (e.g. "sample A aggregated or is 58 nm based on correlation."
+     can get the info as a table output. 
+         a) normalized to highest value
+         b) not normalized 
+    2) Do #1 above AND plot all the spectra and label them. 
+The info in #1 can just be attached to each spectrum rather than in a table, or both. 
+
+TO DO:
+1)put spectra in different csv files and label them. Put a test spectrum for now 
+
+
+"""
+
