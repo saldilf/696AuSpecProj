b754fa9e1d0b9c56fb67282ef5084ce6684407bd
Salwan Butrus <salwan@umich.edu>
2018-11-17 20:29:58 -0500

    user can enter filename

--- AuSpectra.py
+++ AuSpectra.py
@@ -15,25 +15,25 @@ At a minimum, there should be two options that the user can choose to control th
 The info in #1 can just be attached to each spectrum rather than in a table, or both. 
 
 
+Testing: 
+    1) opening file
+    2)
+
 
 """
 
 import numpy as np
-from scipy.integrate import odeint
 import matplotlib.pyplot as plt
 import xlrd
-import pandas as pd
-
-
-#excel_file = 'TemplateData.xlsx'
-#AuData = pd.read_excel(excel_file)
-#AuData.head()
-#print(AuData.shape)
 
 
 
 #read file: this should eventually be an input from user
-path = ('/Users/salwanbutrus/Desktop/Desktop_Organized/ChE696/Project/TemplateData.xlsx')
+
+
+#path = ('/Users/salwanbutrus/Desktop/Desktop_Organized/ChE696/Project/TemplateDataSt400.xlsx')
+#path = ('TemplateDataSt400.xlsx')
+path = input("Enter a file name (e.g. data.xlsx): ")
 wb = xlrd.open_workbook(path)
 sheet1 = wb.sheet_by_index(0)
 
@@ -42,26 +42,32 @@ c = sheet1.ncols
 
 
 
+''' NEXT COMMIT: user should enter their x range and code should adapt: ask "what's the lower and upper limit if your lambdas" '''
+
+xLimLflt = sheet1.cell(1,0).value
+xLimL = int(xLimLflt)
+
+xLimUflt = sheet1.cell(r-1,0).value
+xLimU = int(xLimUflt)
 
 
 #for loop that plots/analyzes based on how many columns
 for x in range(1,c):
-    lambdas = sheet1.col_values(colx = 0,start_rowx = r-301,end_rowx = r) #x-vals
-    abso = sheet1.col_values(colx = x,start_rowx = r-301,end_rowx = r) #y-vals loop cycles thru
     
-    print("{} has a lambdaMax of {} at {} absorbance".format(sheet1.cell(0,x).value,  abso.index(max(abso)) + 400  ,  max(abso)  )   )
+    #299 is the number of data points minus 2
+    lambdas = sheet1.col_values(colx = 0,start_rowx = (xLimU - 299)-xLimL,end_rowx = r) #x-vals
+    abso = sheet1.col_values(colx = x,start_rowx = (xLimU - 299)-xLimL,end_rowx = r) #y-vals loop cycles thru
+    
+    #400 is the lambda we start plotting at
+    print("{} has a lambdaMax of {} at {} absorbance".format(sheet1.cell(0,x).value,  abso.index(max(abso)) + 400  ,  max(abso)  )   ) 
     
     plt.plot(lambdas, abso ,linewidth=2,label= sheet1.cell(0,x).value)
     plt.xlabel(sheet1.cell(0,0).value)
     plt.ylabel('Absorbance')
     plt.legend()
-    plt.title('Not Normalized')
+    plt.title('Not Normalized' )
     axes = plt.gca()
     axes.set_ylim([0 , 1.5])
-
-
-
-
 
 
 
