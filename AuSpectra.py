645007e96ab13bbd7718f54c58e8ee14537182c0
Salwan Butrus <salwan@umich.edu>
2018-11-17 19:34:45 -0500

    further automated plotting and output lambdamax

--- AuSpectra.py
+++ AuSpectra.py
@@ -40,34 +40,55 @@ sheet1 = wb.sheet_by_index(0)
 r = sheet1.nrows
 c = sheet1.ncols
 
-lambdas = sheet1.col_values(colx = 0,start_rowx = r-301,end_rowx = r)
-abso1 = sheet1.col_values(colx = 1,start_rowx = r-301,end_rowx = r)
-abso2 = sheet1.col_values(colx = 2,start_rowx = r-301,end_rowx = r) 
-abso3 = sheet1.col_values(colx = 3,start_rowx = r-301,end_rowx = r) 
-
-xLabel = sheet1.cell(0,0).value
-yLabel1 = sheet1.cell(0,1).value
-yLabel2 = sheet1.cell(0,2).value
-yLabel3 = sheet1.cell(0,3).value
-
 
 
 
 
 #for loop that plots/analyzes based on how many columns
 for x in range(1,c):
-    plt.plot(lambdas,sheet1.col_values(colx = x,start_rowx = r-301,end_rowx = r) ,linewidth=2,label= sheet1.cell(0,x).value)
-    plt.xlabel(xLabel)
+    lambdas = sheet1.col_values(colx = 0,start_rowx = r-301,end_rowx = r) #x-vals
+    abso = sheet1.col_values(colx = x,start_rowx = r-301,end_rowx = r) #y-vals loop cycles thru
+    
+    print("{} has a lambdaMax of {} at {} absorbance".format(sheet1.cell(0,x).value,  abso.index(max(abso)) + 400  ,  max(abso)  )   )
+    
+    plt.plot(lambdas, abso ,linewidth=2,label= sheet1.cell(0,x).value)
+    plt.xlabel(sheet1.cell(0,0).value)
     plt.ylabel('Absorbance')
     plt.legend()
+    plt.title('Not Normalized')
     axes = plt.gca()
     axes.set_ylim([0 , 1.5])
-    
 
 
 
 
 
+
+
+
+#to normalize: 
+#for x in range(0,r-1):
+ #   abso1log.append(abso1[x]/max(abso))
+
+
+
+
+
+
+#plt.savefig('testfig.png', format='png', dpi=1000)
+
+
+#--Hardcode learning---#
+#lambdas = sheet1.col_values(colx = 0,start_rowx = r-301,end_rowx = r) #x-vals
+
+#abso1 = sheet1.col_values(colx = 1,start_rowx = r-301,end_rowx = r)
+#abso2 = sheet1.col_values(colx = 2,start_rowx = r-301,end_rowx = r) 
+#abso3 = sheet1.col_values(colx = 3,start_rowx = r-301,end_rowx = r) 
+
+#xLabel = sheet1.cell(0,0).value
+#yLabel1 = sheet1.cell(0,1).value
+#yLabel2 = sheet1.cell(0,2).value
+#yLabel3 = sheet1.cell(0,3).value
 
 
 #plt.plot(lambdas,abso2,linewidth=2,label=yLabel2)
@@ -80,14 +101,10 @@ for x in range(1,c):
 
 
 
-#to normalize: 
-#for x in range(0,r-1):
- #   abso1log.append(abso1[x]/max(abso))
+
     
 
 
 
-#plot label should be the name of the data in column
 
-    #print(sheet1.cell(0, x))
 
